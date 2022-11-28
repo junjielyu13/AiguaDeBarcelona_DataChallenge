0b0fbf7099fbcce3e97637054deddc8970b976b8
Junjie_Li <jfjunjieli@gmail.com>
2022-11-28 13:57:00 +0100

    update web style

--- static/js/prediction.js
+++ static/js/prediction.js
@@ -85,30 +85,18 @@ function prediction(theform) {
     "VILADECANS",
   ];
 
-  if (types == "comercial") {
-    if (comerciallist.includes(zona)) {
-      analisi_image = path + "/analisi.png";
-      prediction_image = path + "/prediction.png";
-    } else {
-      analisi_image = "../static/update/grafic_2_1.jpg";
-      prediction_image = "../static/update/grafic_2_1.jpg";
-    }
-  } else if (types == "domestico") {
-    if (domestico.includes(zona)) {
-      analisi_image = path + "/analisi.png";
-      prediction_image = path + "/prediction.png";
-    } else {
-      analisi_image = "../static/update/grafic_2_1.jpg";
-      prediction_image = "../static/update/grafic_2_1.jpg";
-    }
-  } else if (types == "industrial") {
-    if (industrial.includes(zona)) {
-      analisi_image = path + "/analisi.png";
-      prediction_image = path + "/prediction.png";
-    } else {
-      analisi_image = "../static/update/grafic_2_1.jpg";
-      prediction_image = "../static/update/grafic_2_1.jpg";
-    }
+  if (types == "comercial" && comerciallist.includes(zona)) {
+    analisi_image = path + "/analisi.png";
+    prediction_image = path + "/prediction.png";
+  } else if (types == "domestico" && domestico.includes(zona)) {
+    analisi_image = path + "/analisi.png";
+    prediction_image = path + "/prediction.png";
+  } else if (types == "industrial" && industrial.includes(zona)) {
+    analisi_image = path + "/analisi.png";
+    prediction_image = path + "/prediction.png";
+  } else {
+    analisi_image = "../static/update/grafic_2_1.jpg";
+    prediction_image = "../static/update/grafic_2_1.jpg";
   }
 
   prediction_box = document.querySelector("#prediction-image");
@@ -117,3 +105,30 @@ function prediction(theform) {
   analisi_box = document.querySelector("#analisi-image");
   analisi_box.src = analisi_image;
 }
+
+// BADALONA
+// BARCELONA
+// BEGUES
+// CASTELLDEFELS
+// CERDANYOLA
+// CORNELLA
+// EL PAPIOL
+// ESPLUGUES
+// GAVA
+// L'HOSPITALET LLOBR.
+// LA LLAGOSTA
+// LES BOTIGUES SITGES
+// MONTCADA I REIXAC
+// MONTGAT
+// PALLEJA
+// SANT ADRIA
+// SANT BOI
+// SANT CLIMENT LLOB.
+// SANT CUGAT
+// SANT FELIU LL.
+// SANT JOAN DESPI
+// SANT JUST DESVERN
+// STA.COLOMA CERVELLO
+// STA.COLOMA GRAMENET
+// TORRELLES LLOBREGAT
+// VILADECANS
