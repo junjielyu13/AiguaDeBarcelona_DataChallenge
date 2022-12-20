4b1126386d37170234dc8798159fad7975b52eda
Junjie_Li <jfjunjieli@gmail.com>
2022-12-20 17:55:56 +0100

    update: modelv4 valid_p to 0.06

--- model/modelv5/model.py
+++ model/modelv5/model.py
@@ -8,7 +8,6 @@ import os
 import plotly.figure_factory as ff
 from neuralprophet import set_random_seed
 import numpy as np
-#from sklearn.preprocessing import StandardScaler
 
 set_random_seed(0)
 
@@ -16,9 +15,9 @@ set_random_seed(0)
 def NProphet(data_location,
              section2,
              period=4,
-             save_prediction=True,
-             save_img=True,
-             save_model=True,
+             save_prediction=False,
+             save_img=False,
+             save_model=False,
              pred_show=False,
              para_show=False,
              threshold_qt=0.95,
@@ -52,13 +51,16 @@ def NProphet(data_location,
         metrics = m.fit(df, freq="M")
     else:
         df = pd.DataFrame({"ds": section1["FECHA"], "y": section1[section2]})
+        print("--------------------------------")
         metrics = m.fit(df, freq="D")  #progress ="plot-all"
 
     if anomaly_detect == True:
         if mensual == True:
+            print("--------------------------------")
             df_train, df_test = m.split_df(df, freq="M", valid_p=0.2)
         else:
             df_train, df_test = m.split_df(df, freq="D", valid_p=0.006)
+
         metrics_test = d.fit(df_train, freq="D",
                              validation_df=df_test)  #progress ="plot-all"
         det_train = d.predict(df_train)
@@ -92,50 +94,18 @@ def NProphet(data_location,
             'y': 0.95,
             'x': 0.5,
             'xanchor': 'center',
-            'yanchor': 'top',
-            'font': {
-                'size': 30
-            },
+            'yanchor': 'top'
         },
-                            legend={
-                                "font": {
-                                    "size": 18
-                                },
-                            },
-                            xaxis={
-                                "tickfont": {
-                                    "size": 22
-                                },
-                                "title": {
-                                    "text": "Meses",
-                                    "font": {
-                                        "size": 24
-                                    },
-                                },
-                            },
-                            yaxis={
-                                "tickfont": {
-                                    "size": 22
-                                },
-                                "title": {
-                                    "text": "Consumo(Litro/Día)",
-                                    "font": {
-                                        "size": 24
-                                    },
-                                },
-                            })
-
+                            xaxis_title="Meses",
+                            yaxis_title="Consumo(Litro/Mes)")
         fig_param.update_layout(
             title={
                 'text':
                 f"Análisis de la tendencia del consumo {actividad} en {section2}",
-                'y': 0.99,
+                'y': 1,
                 'x': 0.5,
                 'xanchor': 'center',
-                'yanchor': 'top',
-                'font': {
-                    'size': 16
-                },
+                'yanchor': 'top'
             })
 
     else:
@@ -146,55 +116,30 @@ def NProphet(data_location,
             'y': 0.95,
             'x': 0.5,
             'xanchor': 'center',
-            'yanchor': 'top',
-            'font': {
-                'size': 30
-            },
+            'yanchor': 'top'
         },
-                            legend={
-                                "font": {
-                                    "size": 18
-                                },
-                            },
-                            xaxis={
-                                "tickfont": {
-                                    "size": 22
-                                },
-                                "title": {
-                                    "text": "Meses",
-                                    "font": {
-                                        "size": 24
-                                    },
-                                },
-                            },
-                            yaxis={
-                                "tickfont": {
-                                    "size": 22
-                                },
-                                "title": {
-                                    "text": "Consumo(Litro/Día)",
-                                    "font": {
-                                        "size": 24
-                                    },
-                                },
-                            })
+                            xaxis_title="Meses",
+                            yaxis_title="Consumo(Litro/Día)")
 
         fig_param.update_layout(
             title={
                 'text':
                 f"Análisis de la tendencia del consumo {actividad} en {section2}",
-                'y': 0.99,
+                'y': 1,
                 'x': 0.5,
                 'xanchor': 'center',
-                'yanchor': 'top',
-                'font': {
-                    'size': 16
-                },
+                'yanchor': 'top'
             })
 
+
+#         hist = go.Histogram(x=pred_train["ds"], y=pred_train["train_mae_loss"])
+#         # kernel density estimation
+#         # fig = ff.create_distplot(pred_train["ds"],pred_train["train_mae_loss"], curve_type='kde')
+
+#apply this threshold of difference in mae to the test set annd find out the anomaly in the test set.
+# det_test["anomaly"] =  det_test.train_mae_loss > .threshold
     if mensual == True:
         anomaly_detect = False
-
     if anomaly_detect == True:
         plt.hist(det_train["train_mae_loss"])
         plt.hist(det_test["test_mae_loss"])
@@ -227,7 +172,7 @@ def NProphet(data_location,
                        y=df_final["yhat1 10.0%"],
                        name='lower bound',
                        line=dict(width=0),
-                       fillcolor='rgba(68, 68, 68, 0.3)',
+                       fillcolor='rgb(102, 102, 102)',
                        marker=dict(color="#444"),
                        fill='tonexty',
                        showlegend=False))
@@ -248,43 +193,7 @@ def NProphet(data_location,
                        mode='lines',
                        name='Actual',
                        line_color="firebrick"))
-        detect.update_layout(title={
-            "text": f"Detección de errores {actividad} en {section2}",
-            'y': 0.95,
-            'x': 0.5,
-            'xanchor': 'center',
-            'yanchor': 'top',
-            'font': {
-                'size': 30
-            },
-        },
-                             legend={
-                                 "font": {
-                                     "size": 18
-                                 },
-                             },
-                             xaxis={
-                                 "tickfont": {
-                                     "size": 22
-                                 },
-                                 "title": {
-                                     "text": "Meses",
-                                     "font": {
-                                         "size": 24
-                                     },
-                                 },
-                             },
-                             yaxis={
-                                 "tickfont": {
-                                     "size": 22
-                                 },
-                                 "title": {
-                                     "text": "Consumo(Litro/Día)",
-                                     "font": {
-                                         "size": 24
-                                     },
-                                 },
-                             })
+        detect.update_layout(title={"text": "Detección de errores"})
         detect.add_scatter(x=det_train[det_train.anomaly == True]["ds"],
                            y=det_train[det_train.anomaly == True]["yhat1"],
                            mode="markers",
@@ -295,13 +204,14 @@ def NProphet(data_location,
                            mode="markers",
                            marker=dict(color="rgb(255,217,47)"),
                            name='potential error detected')
-        # detect.update_layout(template='plotly_dark')
+        detect.update_layout(template='plotly_dark')
         # anomaly.add_scatter(x=det_train.ds, y=det_train.threshold,name='threshold')
         # anomaly.add_scatter(x=det_test.ds, y=det_test[det_test.anomaly== True]["yhat1"],name='anomaly detected')
 
         detect.update_xaxes(
             type="date",
             range=["2021-09-01T00:00:00.000Z", "2022-1-01T00:00:00.000Z"])
+        detect.show()
 
     #Plot lines
     final.add_trace(
@@ -344,111 +254,66 @@ def NProphet(data_location,
                    fill='tonexty',
                    showlegend=False))
 
+    #TODO
+    # Create an animation object
+
+    # frames = [go.Frame(data=[final])]
+    # animation = go.Animation(frames=frames)
+
+    # # Create a figure object and add the animation to it
+    # fig = go.Figure(data=[detect], layout=go.Layout(updatemenus=[dict(type='buttons', buttons=[dict(label='Play', method='animate', args=[None])])]))
+    # fig.update_layout(updatemode='linear')
+    # fig.show()
+
+    # final.show()
     pred_train.to_csv(r"./df_final.csv")
 
     # Saves
-    # if save_prediction == True:
-    #     path = "./model/modelv4/prediction/"
-
-    #     if mensual:
-    #         path += "mensual/"
-    #     elif anomaly_detect == True:
-    #         path += "error/"
-    #     else:
-    #         path += "diario/"
-
-    #     if not os.path.exists(path):
-    #         os.makedirs(path)
-    #     df.to_csv(path + f"result_{actividad}_{section2}.csv")
-
-    # if save_model == True:
-    #     path = "./model/modelv4/save_model/"
-
-    #     if mensual == True:
-    #         path += "mensual/"
-    #     elif anomaly_detect == True:
-    #         path += "error/"
-    #     else:
-    #         path += "diario/"
-
-    #     if not os.path.exists(path):
-    #         os.makedirs(path)
-
-    #     torch.save(m.model.state_dict(),
-    #                path + f"model_{actividad}_{section2}.pth")
-
+    if save_prediction == True:
+        df.to_csv(
+            rf"C:\Users\23675\OneDrive\桌面\ABChallenge\Neuralprophet result\result_{consumo}_{section2}.csv"
+        )
+    if save_model == True:
+        torch.save(
+            m.model.state_dict(),
+            rf"C:\Users\23675\OneDrive\桌面\ABChallenge\Neuralprophet result\model_{consumo}_{section2}.pth"
+        )
     if save_img == True:
-
-        path = "./model/modelv4/images/"
-
-        if mensual == True:
-            path += "mensual/"
-        elif anomaly_detect == True:
-            path += "error/"
-        else:
-            path += "diario/"
-
-        if not os.path.exists(path):
-            os.makedirs(path)
-
-        # fig_param.write_image(path + f"param_{actividad}_{section2}.jpeg")
-        # final.write_image(path + f"prediction_{actividad}_{section2}.jpeg",
-        #                   width=1980,
-        #                   height=1080)
-        if anomaly_detect:
-            detect.write_image(path + f"error_{actividad}_{section2}.jpeg",
-                               width=1980,
-                               height=1080)
-
-    if pred_show == True:
+        final.write_image(
+            rf"C:\Users\23675\OneDrive\桌面\ABChallenge\Neuralprophet result\result_{consumo}_{section2}.jpeg"
+        )
+    if para_show == True:
         fig_param.show()
+    if pred_show == True:
         final.show()
-        if anomaly_detect:
-            detect.show()
-
+    #1
+    if save_model == True:
+        torch.save(
+            m.model.state_dict(),
+            rf"C:\Users\23675\OneDrive\桌面\ABChallenge\Neuralprophet result\model_{consumo}_{section2}.pth"
+        )  #####Saves the model's weights and state in the file model.pth, which allows the continuous update once it is deployed on the website.
     return m, pred_train, pred_next, final, fig_param, metrics  #pred_train -> train + test
 
+mensual_Barcelona_comercial = r".\sum_mensual_comercial.xlsx"
 
-# mensual_Barcelona_comercial = r".\sum_mensual_comercial.xlsx"
-# mensual_Barcelona_industrial = "./model/modelv2/data/sum_mensual_industrial.xlsx"
-# mensual_Barcelona_domestic = r".\sum_mensual_domestic.xlsx"
+mensual_Barcelona_industrial = r".\sum_mensual_industrial.xlsx"
+mensual_Barcelona_domestic = r".\sum_mensual_domestic.xlsx"
 
-# fig_param = {}
+fig_param = {}
 
-# ###Noted that the last available data month(2021 December) is not complete, which shows a significant decay in the representation.
+###Noted that the last available data month(2021 December) is not complete, which shows a significant decay in the representation.
 
-# diario_Barcelona_comercial = "./model/modelv2/data/sum_diario_comercial.xlsx"
+diario_Barcelona_comercial = r"sum_diario_comercial.xlsx"
 
-# diario_Barcelona_industrial = r"sum_diario_industrial.xlsx"
-# diario_Barcelona_domestic = r"sum_diario_domestic.xlsx"
+diario_Barcelona_industrial = r"sum_diario_industrial.xlsx"
+diario_Barcelona_domestic = r"sum_diario_domestic.xlsx"
 
-# param = NProphet(mensual_Barcelona_industrial, "BARCELONA", threshold_qt=0.95)
+# model,pred_train,pred_next,fig_final,fig_param["BARCELONA"],metrics = NProphet(mensual_Barcelona_industrial,"BARCELONA",pred_show = True,para_show = True,threshold_qt=0.95)
 
-# param = NProphet(diario_Barcelona_comercial, "BARCELONA", threshold_qt=0.95)
-
-
-
-data_set = [
-    "./model/modelv2/data/sum_diario_comercial.xlsx",
-    "./model/modelv2/data/sum_diario_domestic.xlsx",
-    "./model/modelv2/data/sum_diario_industrial.xlsx",
-    # "./model/modelv2/data/sum_mensual_comercial.xlsx",
-    # "./model/modelv2/data/sum_mensual_domestic.xlsx",
-    # "./model/modelv2/data/sum_mensual_industrial.xlsx"
-]
-
-zonas = [
-    "BADALONA", "BARCELONA", "BEGUES", "CASTELLDEFELS", "CERDANYOLA",
-    "CORNELLA", "EL PAPIOL", "ESPLUGUES", "GAVA", "L'HOSPITALET LLOBR.",
-    "LA LLAGOSTA", "LES BOTIGUES SITGES", "MONTCADA I REIXAC", "MONTGAT",
-    "PALLEJA", "SANT ADRIA", "SANT BOI", "SANT CLIMENT LLOB.", "SANT CUGAT",
-    "SANT FELIU LL.", "SANT JOAN DESPI", "SANT JUST DESVERN",
-    "STA.COLOMA CERVELLO", "STA.COLOMA GRAMENET", "TORRELLES LLOBREGAT",
-    "VILADECANS"
-]
-
-for data in data_set:
-    original = pd.read_excel(data)
-    for zona in zonas:
-        if zona in original.columns:
-            model = NProphet(data, zona)
+model, pred_train, pred_next, fig_final, fig_param[
+    "BARCELONA"], metrics = NProphet(
+        "./model/modelv2/data/sum_diario_comercial.xlsx",
+        "BARCELONA",
+        pred_show=True,
+        para_show=True,
+        threshold_qt=0.95)
